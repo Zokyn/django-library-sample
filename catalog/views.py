83ad7b2808b2ebc932af4ebc33c67467aeadc0d0
Gustavo Casaes <gustavo.casaes@mail.uft.edu.br>
2023-10-03 16:55:42 -0300

    Criado a primeira view (index) em catalog/

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    # Count of some main objects
+    n_books = Book.objects.all().count()
+    n_instances = BookInstance.objects.all().count()
+
+    # Available books to loan
+    n_instances_available = BookInstance.objects.filter(status__exact='A').count()
+
+    # Total of authors book 
+    n_authors = Author.objects.count()
+
+    context = {
+        'n_books': n_books,
+        'n_instances': n_instances,
+        'n_instances_available': n_instances_available,
+        'n_authors': n_authors
+    }
+    
+    
+    return render(request, 'index.html', context=context)
